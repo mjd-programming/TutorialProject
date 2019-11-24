e99428a11c36c98a1f4111146823192fb0c6e49b
Duc Tran <32504040+dmtran02@users.noreply.github.com>
2019-11-24 17:12:24 -0500

    Add files via upload

---
+++ p5Test/p5Test/wwwroot/assets/js/sketch.js
@@ -0,0 +1,19 @@
+﻿// .save(name.ext);
+
+var img;
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+    strokeWeight(10);
+    stroke(0);
+    
+}
+
+function draw() {
+    
+}
+
+function touchMoved() {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    return false;
+}
